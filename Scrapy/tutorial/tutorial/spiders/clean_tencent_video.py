33080fa4a8e91dd69baf54d1834aafb0e9580490
pulphead <eva.w.xiao@gmail.com>
2018-09-07 20:57:14 +0800

    Add Scrapy dir, includes Python file that scrapes Tencent Video into json (Scrapy spider) and a Python script to clean it and port the results into a CSV

---
+++ Scrapy/tutorial/tutorial/spiders/clean_tencent_video.py
@@ -0,0 +1,45 @@
+#turn json into csv
+# calculate difference between global and local runtime
+import json
+import re
+import csv
+import sys
+
+with open("tencent_video.json") as json_file:
+	data = json.load(json_file)
+
+with open("tencent_video.csv","a", newline='') as csv_file:
+
+	fieldnames = ["English Title", "China Runtime", "Converted Runtime", "China Release"]
+	writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+	writer.writeheader()
+
+	for row in range(0, len(data)):
+
+		#convert runtime
+		if ((data[row]["china_runtime"] != None) and (len(data[row]["china_runtime"])> 0)): #if not an empty string
+			convert_list = re.split(":", data[row]["china_runtime"])
+			for x in range(0,len(convert_list)):
+				if (x == 0):
+					hours = (int(convert_list[0])*60)
+				elif (x == 1):
+					minutes = int(convert_list[1])
+
+			converted_runtime = hours + minutes
+		else:
+			converted_runtime = "null"
+			data[row]["china_runtime"] = "null"
+
+
+		if (data[row]["english_title"] == None):
+			data[row]["english_title"] = "null"
+
+		if (data[row]["china_release_date"] == None):
+			data[row]["china_release_date"] = "null"
+
+		writer.writerow({"English Title": data[row]["english_title"], 
+			"China Runtime": data[row]["china_runtime"], 
+			"Converted Runtime": str(converted_runtime), 
+			"China Release": data[row]["china_release_date"]})
+
+
---
+++ Scrapy/tutorial/tutorial/spiders/scraping_tencent_video_movie_info.py
@@ -0,0 +1,33 @@
+import scrapy
+import re
+import csv
+
+class TencentVideoSpider(scrapy.Spider):
+	name = 'tencentvideo'
+	start_urls =  ['http://v.qq.com/x/list/movie?iarea=3&offset=0']
+
+	def parse(self, response):
+
+		videos_root_URL ="http://v.qq.com/x/list/movie"
+		list_of_movies = response.xpath('//strong[@class="figure_title"]/a/@href').extract() 
+		
+		# iterate through all movie links on page
+		for href in list_of_movies:
+			yield response.follow(href, self.parse_movie)
+
+		next_page = response.xpath('//a[@class="page_next"]/@href').extract_first()
+		next_page = videos_root_URL + next_page
+
+		# iterate through movie list pagination
+		if next_page != None:
+			yield response.follow(next_page, self.parse)
+
+	def parse_movie(self, response):
+
+		yield {
+			'chinese_title': response.xpath('//div[@class="tit"]/text()').extract_first(),
+			'english_title': response.xpath('//span[@class="alias"]/text()').extract_first(),
+			'china_runtime': response.xpath('//div[@class="figure_count"]/span[@class="num"]/text()').extract_first(), 
+			'china_release_date': response.xpath('//meta[@itemprop="datePublished"]/@content').extract_first(),
+		}
+		
